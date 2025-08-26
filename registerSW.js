9ff323793b27bedaec75fe3bed17a8d4aff01bf6
xrip <xrip@xrip.ru>
2025-08-26 19:56:25 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pentagram/dist/sw.js', { scope: '/pentagram/dist/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
