6c0dcf22fa17c9d534d0533a9bb87a24de3f3f4f
xrip <xrip@xrip.ru>
2025-08-27 09:23:59 +0300

    Remove unused STUN server from room-manager configuration

--- src/services/room-manager.js
+++ src/services/room-manager.js
@@ -24,7 +24,6 @@ export class RoomManager {
                     { urls: 'stun:stun.l.google.com:19302' },
                     { urls: 'stun:global.stun.twilio.com:3478' },
                     { urls: 'stun:stun1.l.google.com:19302' },
-                    { urls: 'stun.cloudflare.com:3478' },
                 ]
             },
             turnConfig: {
